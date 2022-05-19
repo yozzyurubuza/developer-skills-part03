a1428606eaab9332d3b6edf6c34c64d8eefbdbc4
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-05-19 09:45:01 +0800

    Revise Temperature Measurement problem

--- script.js
+++ script.js
@@ -10,56 +10,6 @@ console.log(calcAge(1995));
 */
 
 /*
-
-const measureKelvin = function () {
-  const measurement = {
-    type: 'temperature',
-    unit: 'celsius',
-
-    // C) FIX
-    // value: Number(prompt('Degrees celsius:')),
-    value: 10,
-  };
-
-  // B) FIND
-  console.table(measurement);
-
-  //   console.log(measurement.value);
-  //   console.warn(measurement.value);
-  //   console.error(measurement.value);
-
-  const kelvin = measurement.value + 273;
-  return kelvin;
-};
-// A) IDENTIFY
-
-console.log(measureKelvin());
-
-// Using a debugger
-const calcTempAmplitudeBug = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
-
-  let max = 0;
-  let min = 0;
-
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-};
-
-const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
-// A) IDENTIFY
-console.log(amplitudeBug);
-*/
-
-/*
 Given an array of forecasted maximum temperatures, the thermometer displays a string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
 
 Your tasks:
